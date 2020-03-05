23e93c3dc564e7cb89e737ee2af47d675b3afeeb
recigio <recigio@gmail.com>
2020-03-04 21:57:23 -0300

    Criado set inicial com numeros possiveis para sorteio

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ client.on("error", function(error) {
     console.error("Erro ao conectar",error);
 });
 
+//cria set de numeros de 1 99 para sorteio
+for (let numero=1; numero <100; numero++){
+    client.sadd("roleta", numero);
+}
 
 //criar usuarios
 //name: “user01”, bcartela: “cartela:01”, bscore: “score:01”
@@ -13,5 +17,4 @@ client.on("error", function(error) {
 
 //criar scores por usuario
 
-client.set("key", "recigio", redis.print);
-client.get("key", redis.print);
+client.smembers("roleta", redis.print);
